57bd33a5d5a3c4fba04099ddbc4ce65f141474a1
binterryan76 <91400196+binterryan76@users.noreply.github.com>
2024-03-24 21:44:02 -0500

    Changed to file scope namespaces everywhere.

--- GCodeProcessor/Views/MessageBoxView.axaml.cs
+++ GCodeProcessor/Views/MessageBoxView.axaml.cs
@@ -2,22 +2,21 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.VisualTree;
 
-namespace GCodeProcessor.Views
-{
-    public partial class MessageBoxView : UserControl
-    {
-        public MessageBoxView()
-        {
-            InitializeComponent();
-        }
+namespace GCodeProcessor.Views;
 
-        /// <summary>
-        /// Find the parent window and close it if it's a modal dialog.
-        /// </summary>
-        private void Close(object sender, RoutedEventArgs args)
-        {
-            Window? parentWindow = this.GetVisualRoot() as Window;
-            parentWindow?.Close();
-        }
+public partial class MessageBoxView : UserControl
+{
+    public MessageBoxView()
+    {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// Find the parent window and close it if it's a modal dialog.
+    /// </summary>
+    private void Close(object sender, RoutedEventArgs args)
+    {
+        Window? parentWindow = this.GetVisualRoot() as Window;
+        parentWindow?.Close();
     }
 }
